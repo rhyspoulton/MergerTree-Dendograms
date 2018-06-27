Removed the need for any units and corrected the unit conversion for VELOCIraptor
ETF will have units 10^10 Msun for masses, Mpc for distances and km/s for velocities
I have updated the conversion tool for VELOCIraptor to always do this. I have also
added the units to the header so the file is self contained and attributes for each
dataset so it knows its orginal name in the catalogue.

ETFio.py
@@ -11,7 +11,7 @@ except NameError:
 
 
 
-def WriteETFCatalogue(format,opt,treedata,Redshift):
+def WriteETFCatalogue(format,opt,treedata,Redshift,fieldsDict):
 	####################################################################################
 
 
@@ -33,12 +33,12 @@ def WriteETFCatalogue(format,opt,treedata,Redshift):
 	headergroup.attrs["startSnap"] = opt.startSnap
 	headergroup.attrs["endSnap"] = opt.endSnap
 	headergroup.attrs["Nsnaps"] = opt.Nsnaps
-	headergroup.attrs["Munit"] = opt.Munit
 	headergroup.attrs["h"] = opt.h
 	headergroup.attrs["boxsize"] = opt.boxsize
 	headergroup.attrs["HALOIDVAL"] = opt.HALOIDVAL
-	headergroup.attrs["MassDef"] = opt.MassDef
-	headergroup.attrs["RDef"] = opt.RDef
+	headergroup.attrs["Munit"] = "10^10 solarmasses"
+	headergroup.attrs["Lunit"] = "Mpc"
+	headergroup.attrs["Vunit"] = "km/s"
 
 	#Add to the header which file the data is from
 	if(format=="VEL"):
@@ -69,7 +69,10 @@ def WriteETFCatalogue(format,opt,treedata,Redshift):
 		#Put all the data in the snapshot group.
 		for key in treedata[snapKey].keys():
 
-			snapgroup.create_dataset(key,data =treedata[snapKey][key])
+			dataset = snapgroup.create_dataset(key,data =treedata[snapKey][key])
+
+			#Add an attribute to the dataset which gives the dataset's orginal name in the catalogue
+			dataset.attrs["origFieldName"] = fieldsDict[key]
 
 	hdffile.close()
 
@@ -83,7 +86,7 @@ class headerOptions(object):
 			raise KeyError("There is no 'Header' group in the hdffile please make sure there is one")
 
 		#Lets check all the required keys exist
-		requiredHeaders = ["startSnap","startSnap","Nsnaps","Munit","h","boxsize"]
+		requiredHeaders = ["startSnap","startSnap","Nsnaps","h","boxsize"]
 		for key in requiredHeaders:
 			if(key not in hdffile["Header"].attrs): 
 				raise KeyError("There is no value for '%s' in the Header please make sure the header in the file has this" %key)
@@ -91,7 +94,6 @@ class headerOptions(object):
 		self.startSnap = np.array(hdffile["Header"].attrs["startSnap"])	
 		self.endSnap = np.array(hdffile["Header"].attrs["endSnap"])		
 		self.Nsnaps = np.array(hdffile["Header"].attrs["Nsnaps"])	
-		self.Munit = np.array(hdffile["Header"].attrs["Munit"])
 		self.h = np.array(hdffile["Header"].attrs["h"])		
 		self.boxsize = np.array(hdffile["Header"].attrs["boxsize"])
 		self.HALOIDVAL = np.array(hdffile["Header"].attrs["HALOIDVAL"])
@@ -205,10 +207,7 @@ def LoadETFCatalogue(filename,plotOpt):
 		for key in requiredDatasets:
 				treedata[snapKey][key] = np.array(hdffile[snapKey][key])
 
-		if(sizeDataKey=="Mass"):
-			treedata[snapKey]["SizeData"] = np.array(hdffile[snapKey][sizeDataKey]) * opt.Munit/1e10
-		else:
-			treedata[snapKey]["SizeData"] = np.array(hdffile[snapKey][sizeDataKey])
+		treedata[snapKey]["SizeData"] = np.array(hdffile[snapKey][sizeDataKey])
 
 		treedata[snapKey]["ColData"] = np.array(hdffile[snapKey][colDataKey]) 
 		if(plotOpt.overplotdata):

convTreesToETF/convVELOCIraptor.py
@@ -57,6 +57,12 @@ def LoadVELOCIraptor(startSnap,endSnap,filename,fieldKeys):
 
 	Redshift = np.zeros(numsnaps,dtype=float)
 
+	# Read the header information
+	icomove = hdffile["Header"]["Units"].attrs["Comoving_or_Physical"][...]
+	length_unit_in_mpc = hdffile["Header"]["Units"].attrs["Length_unit_to_kpc"][...] / 1000.0
+	mass_unit_in_10E10solarmass = hdffile["Header"]["Units"].attrs["Mass_unit_to_solarmass"][...] /1e10
+	velocity_unit_in_kms = hdffile["Header"]["Units"].attrs["Velocity_unit_to_kms"][...]
+
 	# Lets loop over snapshot and extract the necessary data sets
 	for snap in range(startSnap,endSnap+1):
 
@@ -72,12 +78,28 @@ def LoadVELOCIraptor(startSnap,endSnap,filename,fieldKeys):
 		#Loop over all the fields and extract them into memory
 		for field in fieldKeys:
 			
-			if(field=="Pos"):
-				halodata[snapKey][field] = np.column_stack([hdffile[snapKey]["Xc"][:],hdffile[snapKey]["Yc"][:],hdffile[snapKey]["Zc"][:]]) *h/np.float(hdffile[snapKey].attrs["scalefactor"])
-			elif(field=="Rvir"):
-				halodata[snapKey][field] =hdffile[snapKey][field][:] *h/np.float(hdffile[snapKey].attrs["scalefactor"])
-			elif(field=="Vel"):
-				halodata[snapKey][field] = np.column_stack([hdffile[snapKey]["VXc"][:],hdffile[snapKey]["VYc"][:],hdffile[snapKey]["VZc"][:]])
+			#Do all the nessary conversions into ETF units
+			if("Mass" in field):
+				halodata[snapKey][field] = hdffile[snapKey][field][:] * mass_unit_in_10E10solarmass
+
+			elif(field=="Pos"):
+
+				if(icomove):
+					halodata[snapKey][field] = np.column_stack([hdffile[snapKey]["Xc"][:],hdffile[snapKey]["Yc"][:],hdffile[snapKey]["Zc"][:]]) * length_unit_in_mpc
+				else:
+					halodata[snapKey][field] = np.column_stack([hdffile[snapKey]["Xc"][:],hdffile[snapKey]["Yc"][:],hdffile[snapKey]["Zc"][:]]) *h/np.float(hdffile[snapKey].attrs["scalefactor"]) * length_unit_in_mpc
+
+			elif((field=="Radius") | (field[0:2]=="R_") | (field=="Rmax")) :
+
+				if(icomove):
+					halodata[snapKey][field] =hdffile[snapKey][field][:] * length_unit_in_mpc
+				else:
+					halodata[snapKey][field] =hdffile[snapKey][field][:] *h/np.float(hdffile[snapKey].attrs["scalefactor"]) * length_unit_in_mpc
+
+			elif((field=="Vel") | (field=="Vmax") | (field=="sigV")):
+
+				halodata[snapKey][field] = np.column_stack([hdffile[snapKey]["VXc"][:],hdffile[snapKey]["VYc"][:],hdffile[snapKey]["VZc"][:]]) * velocity_unit_in_kms
+
 			else:
 				halodata[snapKey][field] = hdffile[snapKey][field][:]
 

plotDendogram.py
@@ -158,7 +158,6 @@ def plotDendogram(plotOpt,plotData,depthIndicator,branchIndicator,sortIndx,SelID
 	plotData["xposData"][plotData["xposData"]==-1.0] = plotOpt.plotNumRvir 
 
 	maxBranchSize = np.amax(plotData["SizeData"],axis=0)
-
 	#Find the number of branches left and the first and final snapshot in the data
 	numBranches = plotData["xposData"].shape[1] 
 	if(numBranches ==1):
@@ -361,13 +360,13 @@ def plotDendogram(plotOpt,plotData,depthIndicator,branchIndicator,sortIndx,SelID
 			ibranchSel+=1
 
 	#Add a box around the maximum size data and a label
-	patch = Rectangle((0.001,0.95),0.985,0.03,fill=False,transform=fig.transFigure,clip_on=False,lw=2)
+	patch = Rectangle((0.001,0.95),0.998,0.03,fill=False,transform=fig.transFigure,clip_on=False,lw=2)
 	axes[0].add_patch(patch)
 	axes[0].text(-0.25,1.07,plotOpt.sizeLabel,fontsize=plotOpt.maxSizeFontSize,transform=axes[0].transAxes)#,bbox=dict(facecolor='none', edgecolor='black',linewidth=2,pad=1))
 
 	#Add the labels to the main branch of interest
 	axes[0].set_ylabel("Snapshot",fontsize=25)
-	axes[0].set_xlabel("Euclidean distance [%s]" %(plotOpt.DistUnit),fontsize=25)
+	axes[0].set_xlabel("Euclidean distance [Mpc]",fontsize=25)
 	axes[0].margins(x=0.0)
 	axes[0].set_xlim(0,maxDist)
 	for label in axes[i].xaxis.get_ticklabels()[::2]: label.set_visible(False)