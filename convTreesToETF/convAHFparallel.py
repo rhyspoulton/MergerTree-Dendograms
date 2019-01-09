Small bug fix and update to the units in the AHF conversion tool

convTreesToETF/convAHFparallel.py
@@ -107,7 +107,7 @@ def ReadInHaloFilesAcrossSnapshots(opt,fieldsDict):
 
 				elif(MTFfieldname=="Pos"):
 					splitfields  = fieldname.split(",")
-					halodata[snapKey][MTFfieldname]=np.column_stack([tmpData[splitfield] for splitfield in splitfields])/1000
+					halodata[snapKey][MTFfieldname]=np.column_stack([tmpData[splitfield] for splitfield in splitfields])
 
 				else:
 					splitfields  = fieldname.split(",")
@@ -672,8 +672,8 @@ def convToMTF(opt,halodata,treedata,HALOIDVAL = 1000000000000):
 		snapKey = "Snap_%03d" %snap
 
 		MTFdata[snapKey]["Mass"] = halodata[snapKey]["Mass"]/1e10
-		MTFdata[snapKey]["Radius"] = halodata[snapKey]["Radius"]
-		MTFdata[snapKey]["Pos"] = halodata[snapKey]["Pos"]
+		MTFdata[snapKey]["Radius"] = halodata[snapKey]["Radius"]/1000
+		MTFdata[snapKey]["Pos"] = halodata[snapKey]["Pos"]/1000
 		for extraField in extraFields:
 				MTFdata[snapKey][extraField]  = halodata[snapKey][extraField]
 

createPlotArrays.py
@@ -345,7 +345,7 @@ def createPlotArrays(opt,plotOpt,treedata,SelIndex,outdir='',outputArrays=False)
 					plotData["xposData"][isnap,ibranch] = 1e-5
 				#Otherwise track its position relative to its start position
 				else:
-					plotData["xposData"][snap,ibranch] = np.sqrt((np.sum((pos - startpos))**2))
+					plotData["xposData"][isnap,ibranch] = np.sqrt((np.sum((pos - startpos))**2))
 
 				#Store the main branches IDs
 				mainBranchIDs[isnap] = haloID